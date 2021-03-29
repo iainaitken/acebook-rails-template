1818594059e7a93f6b59df8b352134caab67d45a
spaek14 <>
2021-03-29 17:32:37 +0100

    aaaaa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(sign_up_params)
+    if @user.save
+      redirect_to posts_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'users#index'
+  
+  get '/sign_up', to: 'users#new'
+
+  post '/sign_up', to: 'users#create'
 
   resources :posts
 end
