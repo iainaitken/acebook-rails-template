fffe062455fa7a7bdcb536ae970a16c8ce9a48cd
rhc07 <rorieclarke@gmail.com>
2021-03-30 14:53:13 +0100

    error messages on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,9 @@ class UsersController < ApplicationController
   def create
     @new_user = User.create(sign_up_params)
     if @new_user.save
-      p "user saved"
       redirect_to posts_url
     else
-      p "print else activated"
+      flash[:alert] = "Something went wrong, please try again!"
       render :new
     end
   end
