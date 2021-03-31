6b7dcf88524a4201fe34a7629063b908dab03037
rhc07 <rorieclarke@gmail.com>
2021-03-31 11:13:52 +0100

    devise works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true # turn off as per spec
+  # validates :name, presence: true # turn off as per spec
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :password, presence: true, length: { minimum: 6, maximum: 10}
 end
